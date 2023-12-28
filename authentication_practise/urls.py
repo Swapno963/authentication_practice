c6a5f15313e30cc4f4200d2887b74dfa79f93548
swapno963 <swapno963@gmail.com>
2023-12-29 00:07:13 +0600

    a

---
+++ authentication_practise/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for authentication_practise project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/',views.user_login, name ='user_longin'),
+    path('signup/',views.register, name ='signup'),
+    path('logout/',views.user_logout, name ='user_logout'),
+    path('profile/',views.user_profile, name ='profile'),
+    path('profile/pass_change',views.pass_change, name ='pass_change'),
+    path('profile/pass_change_wihtout',views.pass_chang_without, name ='pass_change_without'),
+]
+
---
+++ authentication_practise/views.py
@@ -0,0 +1,75 @@
+from . import forms 
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = forms.RegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Register Successfully.')
+            return redirect('profile ')
+    else:
+        register_form = forms.RegistrationForm()
+    return render(request, 'register.html',{'form':register_form, 'type':'Register'})
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username=user_name, password = user_pass)
+            if user is not None:
+                login(request, user)
+                messages.success(request,'Login Successfylly.')
+                return redirect('profile')
+            else:
+                messages.warning(request, 'Login Information Is Incorrenct')
+                return redirect('profile')
+            
+    else:
+        form = AuthenticationForm()
+    return render(request, 'register.html', {'form':form, 'type':'Login'})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'user_profile.html',{'user':request.user})
+    
+
+def user_logout(request):
+    logout(request)
+    return redirect('user_longin')
+
+
+def pass_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Password Updated Successfully.')
+            update_session_auth_hash(request, form.user)
+            return redirect('profile')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'pass_change.html',{'form':form})
+
+def pass_chang_without(request):
+    if request.method == 'POST':
+        form = SetPasswordForm(request.user, data = request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.usesr)
+            return redirect('user_longin')
+        
+    else:
+        form = SetPasswordForm(user=request.user)
+    return render(request, 'pass_change_without.html',{'form':form})
